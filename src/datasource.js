cbbf8a287350bfe5e4c7d15ceaa040cfbe5e0d57
Christian Fetzer <fetzer.ch@gmail.com>
2019-03-09 09:06:44 +0100

    datasource: Change annotation tags to a list
    Unlike described in the documentation, the annotation query must return
    the tags as a list, not as a string. Returning a string leads to one
    tag per character.

--- src/datasource.js
+++ src/datasource.js
@@ -70,93 +70,93 @@ export class SunAndMoonDatasource {
       sunrise: {
         title: "Sunrise",
         text:  "Top edge of the sun appears on the horizon",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       sunriseEnd: {
         title: "Sunrise ends",
         text:  "Bottom edge of the sun touches the horizon",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       goldenHourEnd: {
         title: "Morning golden hour ends",
         text:  "Soft light, best time for photography",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       solarNoon: {
         title: "Solar noon",
         text:  "Sun is in the highest position",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       goldenHour: {
         title: "Evening golden hour starts",
         text:  "Soft light, best time for photography",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       sunsetStart: {
         title: "Sunset starts",
         text:  "Bottom edge of the sun touches the horizon",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       sunset: {
         title: "Sunset",
         text:  "Sun disappears below the horizon, " +
                "evening civil twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       dusk: {
         title: "Dusk",
         text:  "Evening nautical twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       nauticalDusk: {
         title: "Nautical dusk",
         text:  "Evening astronomical twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       night: {
         title: "Night starts",
         text:  "Dark enough for astronomical observations",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       nadir: {
         title: "Nadir",
         text:  "Darkest moment of the night, sun is in the lowest position",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       nightEnd: {
         title: "Night ends",
         text:  "Morning astronomical twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       nauticalDawn: {
         title: "Nautical dawn",
         text:  "Morning nautical twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       dawn: {
         title: "Dawn",
         text:  "Morning nautical twilight ends, morning civil twilight starts",
-        tags:  "sun"
+        tags:  ["sun"]
       },
       moonrise: {
         title: "Moonrise",
         text:  "Top edge of the moon appears on the horizon",
-        tags:  "moon"
+        tags:  ["moon"]
       },
       moonset: {
         title: "Moonset",
         text:  "Moon disappears below the horizon",
-        tags:  "moon"
+        tags:  ["moon"]
       },
       noon: {
         title: "Noon",
         text: "12 o'clock in the daytime",
-        tags: "time"
+        tags: ["time"]
       },
       midnight: {
         title: "Midnight",
         text: "12 o'clock in the night",
-        tags: "time"
+        tags: ["time"]
       },
     };
   }
