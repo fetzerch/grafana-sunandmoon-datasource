00ac06224ffedae12d9a4abbe14e11556e35946c
Christian Fetzer <fetzer.ch@gmail.com>
2019-08-01 19:31:05 +0200

    project: Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "none",
